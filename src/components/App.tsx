9c77a695a5f44d14bebda4bde828c65fbf7157f6
CodyBrewer <brewer.cody.m@gmail.com>
2020-02-04 17:14:55 -0700

    added drawer toggling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -19,24 +25,81 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const App: React.FC = () => {
   const classes = useStyles();
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const toggleDrawer = (open: boolean) => (
+  //   event: React.KeyboardEvent | React.MouseEvent
+  // ) => {
+  //   if (
+  //     event.type === "keydown" &&
+  //     ((event as React.KeyboardEvent).key === "Tab" ||
+  //       (event as React.KeyboardEvent).key === "Shift")
+  //   ) {
+  //     setIsOpen(open);
+  //   }
+  // };
+
+  // return (
+  //   <div>
+  //     <div className={classes.root}>
+  //       <AppBar position="static">
+  //         <Toolbar>
+  //           <IconButton
+  //             edge="start"
+  //             className={classes.menuButton}
+  //             color="inherit"
+  //             aria-label="menu"
+  //           >
+  //             <MenuIcon />
+  //           </IconButton>
+  //           <Typography variant="h6" className={classes.title}>
+  //             Football Almanac
+  //           </Typography>
+  //         </Toolbar>
+  //       </AppBar>
+  //     </div>
+  //     <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+  //       Hello Drawer!
+  //     </Drawer>
+  //   </div>
+  // );
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      event.type === "keydown" &&
+      ((event as React.KeyboardEvent).key === "Tab" ||
+        (event as React.KeyboardEvent).key === "Shift")
+    ) {
+      return;
+    }
+    setIsOpen(open);
+  };
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Football Almanac
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <div>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Football Almanac
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+        Hello Drawer!
+      </Drawer>
     </div>
   );
 };
